04b0052a0138e5d9d2534ce344ffe5ecbd0e3bd8
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-01-25 21:33:23 -0300

    atualizacao arquivo exercicio1.js

--- java_script_sintaxes_e_variaveis/aula_2_variaveis/script.js
+++ java_script_sintaxes_e_variaveis/aula_2_variaveis/script.js
@@ -4,3 +4,15 @@
 
 //number
 
+//var numeroqualquer = 1;
+//console.log(typeof(numeroqualquer));
+
+//var string nome="Luiz Francisco Marques de Toledo Silva";
+//console.log(nome);
+//console.log(typeof(nome));
+
+//var adicao=1+1;
+//console.log(adicao);
+
+var subtracao = 1 - 1;
+console.log(subtracao);
