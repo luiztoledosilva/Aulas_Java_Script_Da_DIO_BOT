10cf2709626ec59d8178ca6e959dc82a2d9f1803
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-02-05 21:23:16 -0300

    arquivos sobre array spread e rest

---
+++ funcoes_js/array_spread.js
@@ -0,0 +1,18 @@
+/*function sum(x, y, z) {
+
+    return x + y + z;
+
+}
+
+const numbers = [1, 2, 3];
+
+console.log(sum(...numbers));
+*/
+
+function confereTamanho(...args) {
+
+    console.log(args.lenght)
+}
+//confereTamanho()
+//confereTamanho(1, 2)
+confereTamanho(3, 4, 5)
